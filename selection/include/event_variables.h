a9c542eb287258a5f8f33152b6778bd667cef77b
justinjmueller <justin.mueller@colostate.edu>
2025-07-01 15:19:10 -0500

    Add more event variables related to the trigger

--- selection/include/event_variables.h
+++ selection/include/event_variables.h
@@ -48,6 +48,73 @@ namespace evar
     REGISTER_VAR_SCOPE(RegistrationScope::Event, nreco, nreco);
 
     /**
+     * @brief Variable for the time of the global trigger.
+     * @details This variable returns the time of the global trigger in Unix
+     * epoch format (nanoseconds since 1970-01-01T00:00:00Z). This is useful
+     * for dividing the dataset into different "epochs" based on the absolute
+     * time of the global trigger.
+     * @tparam T the top-level record.
+     * @param sr the StandardRecord to apply the variable on.
+     * @return the time of the global trigger in nanoseconds
+     * since 1970-01-01T00:00:00Z.
+     */
+    template<typename T>
+    double global_trigger_time(const T & sr) { return sr.hdr.triggerinfo.global_trigger_time; }
+    REGISTER_VAR_SCOPE(RegistrationScope::Event, global_trigger_time, global_trigger_time);
+
+    /**
+     * @brief Variable for the time of the beam gate in UTC
+     * @details This variable returns the time of the beam gate in the absolute
+     * time system in nanoseconds since 1970-01-01T00:00:00Z.
+     * @tparam T the top-level record.
+     * @param sr the StandardRecord to apply the variable on.
+     * @return the time of the beam gate in nanoseconds since
+     * 1970-01-01T00:00:00Z.
+     */
+    template<typename T>
+    double beam_gate_time_abs(const T & sr) { return sr.hdr.triggerinfo.beam_gate_time_abs; }
+    REGISTER_VAR_SCOPE(RegistrationScope::Event, beam_gate_time_abs, beam_gate_time_abs);
+
+    /**
+     * @brief Variable for the time of the trigger within the beam gate.
+     * @details This variable returns the time of the trigger within the beam
+     * gate in microseconds.
+     * @tparam T the top-level record.
+     * @param sr the StandardRecord to apply the variable on.
+     * @return the time of the trigger within the beam gate in microseconds.
+     */
+    template<typename T>
+    double trigger_within_gate(const T & sr) { return sr.hdr.triggerinfo.trigger_within_gate; }
+    REGISTER_VAR_SCOPE(RegistrationScope::Event, trigger_within_gate, trigger_within_gate);
+
+    /**
+     * @brief Variable for the time of the beam gate in the detector time
+     * system.
+     * @details This variable returns the time of the beam gate in the
+     * detector time system in microseconds.
+     * @tparam T the top-level record.
+     * @param sr the StandardRecord to apply the variable on.
+     * @return the time of the beam gate in the detector time system in
+     */
+    template<typename T>
+    double beam_gate_det_time(const T & sr) { return sr.hdr.triggerinfo.beam_gate_det_time; }
+    REGISTER_VAR_SCOPE(RegistrationScope::Event, beam_gate_det_time, beam_gate_det_time);
+
+    /**
+     * @brief Variable for the time of the global trigger in the detector time
+     * system.
+     * @details This variable returns the time of the global trigger in the
+     * detector time system in microseconds.
+     * @tparam T the top-level record.
+     * @param sr the StandardRecord to apply the variable on.
+     * @return the time of the global trigger in the detector time system in
+     * microseconds.
+     */
+    template<typename T>
+    double global_trigger_det_time(const T & sr) { return sr.hdr.triggerinfo.global_trigger_det_time; }
+    REGISTER_VAR_SCOPE(RegistrationScope::Event, global_trigger_det_time, global_trigger_det_time);
+
+    /**
      * @brief Variable for time of the flash closest to the trigger time.
      * @details This variable is intended to provide the time of the flash
      * closest to the trigger time of the event. It is useful for producing a
@@ -72,24 +139,6 @@ namespace evar
         return closest_flash_to_trigger + t0;
     }
     REGISTER_VAR_SCOPE(RegistrationScope::Event, time_of_flash_closest_to_trigger, time_of_flash_closest_to_trigger);
-
-    /**
-     * @brief Variable for the time of the global trigger.
-     * @details This variable returns the time of the global trigger in Unix
-     * epoch format (nanoseconds since 1970-01-01T00:00:00Z). This is useful
-     * for dividing the dataset into different "epochs" based on the absolute
-     * time of the global trigger.
-     * @tparam T the top-level record.
-     * @param sr the StandardRecord to apply the variable on.
-     * @return the time of the global trigger in nanoseconds
-     * since 1970-01-01T00:00:00Z.
-     */
-    template<typename T>
-    double global_trigger_time(const T & sr)
-    {
-        return sr.hdr.triggerinfo.global_trigger_time;
-    }
-    REGISTER_VAR_SCOPE(RegistrationScope::Event, global_trigger_time, global_trigger_time);
 }
 
 #endif
