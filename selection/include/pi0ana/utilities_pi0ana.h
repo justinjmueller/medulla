bf94777959300e0d55e6144459a04abe6b04a952
Lane Kashur <lkashur@icarusgpvm02.fnal.gov>
2025-11-13 19:38:17 -0600

    Tweak ccpi0 signal definition

--- selection/include/pi0ana/utilities_pi0ana.h
+++ selection/include/pi0ana/utilities_pi0ana.h
@@ -100,13 +100,15 @@ namespace utilities_pi0ana
 	      num_pi0_daughters++;
 	    }
 	    pi0_ke = std::sqrt(std::pow(PI0_MASS, 2) + std::pow(pi0_momentum.Mag(), 2)) - PI0_MASS;
-            if(num_pi0_daughters < 2 || pi0_ke < params[0]) bad_pi0_ids.push_back(_pi0.first);
+	    
+	    // TEST: RELAX SIGNAL DEFINITION
+            //if(num_pi0_daughters < 2 || pi0_ke < params[0]) bad_pi0_ids.push_back(_pi0.first);
+	    if (pi0_ke < params[0]) bad_pi0_ids.push_back(_pi0.first);
 	}
-	// TEST: RELAX SIGNAL DEFINITION
-	//for(size_t i=0; i<bad_pi0_ids.size(); i++)
-	//{
-        //    true_pi0s.erase(bad_pi0_ids[i]);
-	//}
+	for(size_t i=0; i<bad_pi0_ids.size(); i++)
+	{
+            true_pi0s.erase(bad_pi0_ids[i]);
+	}
 	return true_pi0s;
     }
 
@@ -271,6 +273,9 @@ namespace utilities_pi0ana
 			   s.vertex_y = vertex_y;
 			   s.vertex_z = vertex_z;
 			   s.momentum = momentum.Mag()/1000.0;
+			   // Override if only one pi0 daughter exists
+			   if (num_pi0_daughters < 2)
+			     s.momentum = -5;
 			   s.beam_costheta = beam_costheta;
 			   s.leading_shower_index = leading_shower_index;
 			   s.leading_shower_ke = leading_shower_ke/1000.0;
