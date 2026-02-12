7261f5a075d972daea82c981018a9df455fb1af7
Nupur Oza <noza@sbndgpvm03.fnal.gov>
2026-02-12 16:46:02 -0600

    remove other changes

--- batch/submit.sh
+++ batch/submit.sh
@@ -63,7 +63,7 @@ ups active
 # Build medulla
 git clone https://github.com/justinjmueller/medulla.git
 cd medulla
-git checkout feature/nue
+git checkout develop
 mkdir build && cd build
 export CC=$(which gcc)
 export CXX=$(which g++)
