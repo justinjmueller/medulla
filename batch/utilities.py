e68fd30e841be5166f6db4fe44aa889c0f922757
JosiePaton <44001819+JosiePaton@users.noreply.github.com>
2026-04-15 15:47:08 -0500

    Returning job length to 1hr
    Apologies for messy git practices

--- batch/utilities.py
+++ batch/utilities.py
@@ -401,7 +401,7 @@ def launch_jobsub(
         '-N', str(njobs),
         '--memory=1800MB',
         f'--disk={"10GB" if exp == "sbnd" else "25GB"}',
-        '--expected-lifetime=4h',
+        '--expected-lifetime=1h',
         '--resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE',
         "--append_condor_requirements='(TARGET.HAS_Singularity==true)'",
         '--singularity-image=/cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:latest',
