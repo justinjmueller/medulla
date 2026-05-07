3e664f4405d24868eec79f5fd35f576c7e1acb2b
Guadalupe Moreno Granados <lupis.mo05@gmail.com>
2026-05-07 11:53:39 -0500

    commenting out the default tag section in the submit file

--- batch/submit.sh
+++ batch/submit.sh
@@ -23,16 +23,14 @@ while [[ $# -gt 0 ]]; do
       PROJECT="$2"
       shift 2
       ;;
-'''
-    --tag=*)
-      TAG="${1#*=}"
-      shift
-      ;;
-    --tag)
-      TAG="$2"
-      shift 2
-      ;;
-'''
+#    --tag=*)
+#      TAG="${1#*=}"
+#      shift
+#      ;;
+#    --tag)
+#      TAG="$2"
+#      shift 2
+#      ;;
     -h|--help)
       usage
       ;;
