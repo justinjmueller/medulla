ee9584484cc30f21dee8b6e9099ca691c18fcfe6
Guadalupe Moreno Granados <lupis.mo05@gmail.com>
2026-05-08 10:07:18 -0500

    This is a test: changing the tag (probably I'll have to change it one more time.

--- batch/utilities.py
+++ batch/utilities.py
@@ -364,8 +364,8 @@ def launch_jobsub(
     exp : str = 'sbnd',
     njobs : int = -1,
     confirm : bool = True,
-    #tag : str = 'develop',
-    tag : str = 'feature/lupis_nuEES',
+    tag : str = 'develop',
+    #tag : str = 'feature/lupis_nuEES',
 ):
     """
     Launch jobs using jobsub for the given project directory. If njobs
