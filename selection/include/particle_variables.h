09eec360198063babef0699075a8b04e0bd57cf9
Brandon Weiss <bmweiss10@gmail.com>
2026-07-31 13:41:03 -0500

    remove duplicate variable definition for default_calo_ke

--- selection/include/particle_variables.h
+++ selection/include/particle_variables.h
@@ -268,21 +268,6 @@ namespace pvars
     extern std::shared_ptr<VarFn<T>> calofn;
 
     /**
-     * @brief Variable for the default calorimetric kinetic energy of the particle.
-     * @details The calorimetic kinetic energy is calculated upstream in the
-     * SPINE reconstruction as the sum of energy for each spacepoint in the
-     * particle.
-     * @param p the particle to apply the variable on.
-     * @return the calorimetric kinetic energy of the particle.
-     */
-    template<class T>
-    double default_calo_ke(const T & p)
-    {
-        return p.calo_ke;
-    }
-    REGISTER_VAR_SCOPE(RegistrationScope::RecoParticle, default_calo_ke, default_calo_ke);
-
-    /**
      * @brief Variable for the calorimetric kinetic energy of the particle.
      * @details The calorimetic kinetic energy is calculated upstream in the
      * SPINE reconstruction as the sum of energy of each spacepoint in the
